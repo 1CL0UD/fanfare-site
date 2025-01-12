4d66f54d64d26bac177c93991209891dbf971cae
1CL0UD <1CL0UD>
2025-01-13 00:49:02 +0700

    fixed header dark mode

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -20,13 +20,12 @@ const Header = () => {
   const user = useUser();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
+  const { theme, setTheme } = useTheme();
 
   const isActive = (href: string) => pathname === href;
 
-  const { setTheme } = useTheme();
-
   return (
-    <header className="bg-white border-b border-gray-200">
+    <header className="bg-white dark:bg-gray-800 border-b dark:border-gray-700">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center">
@@ -37,14 +36,15 @@ const Header = () => {
                 width={96}
                 height={48}
                 alt="Fanfare Logo"
+                className="dark:invert"
               />
             </Link>
             <nav className="hidden md:ml-6 md:flex md:space-x-8">
               <Link
                 href="/"
-                className={`text-gray-500 hover:text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium ${
+                className={`text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white inline-flex items-center px-1 pt-1 text-sm font-medium ${
                   isActive('/')
-                    ? 'text-gray-900 border-b-2 border-gray-900 pb-2'
+                    ? 'text-gray-900 dark:text-white border-b-2 border-gray-900 dark:border-white pb-2'
                     : ''
                 }`}
               >
@@ -53,9 +53,9 @@ const Header = () => {
               </Link>
               <Link
                 href="/network"
-                className={`text-gray-500 hover:text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium ${
+                className={`text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white inline-flex items-center px-1 pt-1 text-sm font-medium ${
                   isActive('/network')
-                    ? 'text-gray-900 border-b-2 border-gray-900 pb-2'
+                    ? 'text-gray-900 dark:text-white border-b-2 border-gray-900 dark:border-white pb-2'
                     : ''
                 }`}
               >
@@ -64,9 +64,9 @@ const Header = () => {
               </Link>
               <Link
                 href="/messages"
-                className={`text-gray-500 hover:text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium ${
+                className={`text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white inline-flex items-center px-1 pt-1 text-sm font-medium ${
                   isActive('/messages')
-                    ? 'text-gray-900 border-b-2 border-gray-900 pb-2'
+                    ? 'text-gray-900 dark:text-white border-b-2 border-gray-900 dark:border-white pb-2'
                     : ''
                 }`}
               >
@@ -76,7 +76,7 @@ const Header = () => {
             </nav>
           </div>
           <div className="hidden md:flex items-center">
-            <button className="p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            <button className="p-1 rounded-full text-gray-400 dark:text-gray-300 hover:text-gray-500 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
               <span className="sr-only">View notifications</span>
               <Bell className="h-6 w-6" />
             </button>
@@ -84,16 +84,12 @@ const Header = () => {
               extraItems={[
                 {
                   text: 'Light Mode',
-
                   icon: <Sun />,
-
                   onClick: () => setTheme('light'),
                 },
                 {
                   text: 'Dark mode',
-
                   icon: <Moon />,
-
                   onClick: () => setTheme('dark'),
                 },
               ]}
@@ -102,7 +98,7 @@ const Header = () => {
           <div className="flex md:hidden">
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-300 hover:text-gray-500 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
             >
               <span className="sr-only">Open main menu</span>
               <Menu className="block h-6 w-6" aria-hidden="true" />
@@ -115,54 +111,62 @@ const Header = () => {
           <div className="pt-2 pb-3 space-y-1">
             <Link
               href="/"
-              className="bg-blue-50 border-blue-500 text-blue-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+              className={`${
+                isActive('/')
+                  ? 'bg-blue-50 dark:bg-blue-900 border-blue-500 dark:border-blue-500 text-blue-700 dark:text-blue-300'
+                  : 'border-transparent text-gray-500 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 hover:text-gray-700 dark:hover:text-white'
+              } block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6`}
             >
               Home
             </Link>
             <Link
               href="/network"
-              className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+              className={`${
+                isActive('/network')
+                  ? 'bg-blue-50 dark:bg-blue-900 border-blue-500 dark:border-blue-500 text-blue-700 dark:text-blue-300'
+                  : 'border-transparent text-gray-500 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 hover:text-gray-700 dark:hover:text-white'
+              } block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6`}
             >
               Network
             </Link>
             <Link
               href="/messages"
-              className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+              className={`${
+                isActive('/messages')
+                  ? 'bg-blue-50 dark:bg-blue-900 border-blue-500 dark:border-blue-500 text-blue-700 dark:text-blue-300'
+                  : 'border-transparent text-gray-500 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 hover:text-gray-700 dark:hover:text-white'
+              } block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6`}
             >
               Messages
             </Link>
           </div>
-          <div className="pt-4 pb-3 border-t border-gray-200">
+          <div className="pt-4 pb-3 border-t border-gray-200 dark:border-gray-700">
             <div className="flex items-center px-4 sm:px-6">
               <div className="flex-shrink-0">
                 <UserButton
                   extraItems={[
                     {
                       text: 'Light Mode',
-
                       icon: <Sun />,
-
                       onClick: () => setTheme('light'),
                     },
                     {
                       text: 'Dark mode',
-
                       icon: <Moon />,
-
                       onClick: () => setTheme('dark'),
                     },
                   ]}
                 />
               </div>
               <div className="ml-3">
-                <div className="text-base font-medium text-gray-800">
+                <div className="text-base font-medium text-gray-800 dark:text-white">
                   {user?.displayName}
                 </div>
-                <div className="text-sm font-medium text-gray-500">
+                <div className="text-sm font-medium text-gray-500 dark:text-gray-400">
                   {user?.primaryEmail}
                 </div>
               </div>
-              <button className="ml-auto flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              <button className="ml-auto flex-shrink-0 p-1 rounded-full text-gray-400 dark:text-gray-300 hover:text-gray-500 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
                 <span className="sr-only">View notifications</span>
                 <Bell className="h-6 w-6" aria-hidden="true" />
               </button>
@@ -170,19 +174,19 @@ const Header = () => {
             <div className="mt-3 space-y-1">
               <Link
                 href="/profile"
-                className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
+                className="block px-4 py-2 text-base font-medium text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700 sm:px-6"
               >
                 Profile
               </Link>
               <Link
                 href="/settings"
-                className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
+                className="block px-4 py-2 text-base font-medium text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700 sm:px-6"
               >
                 Settings
               </Link>
               <Link
                 href="/signout"
-                className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
+                className="block px-4 py-2 text-base font-medium text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700 sm:px-6"
               >
                 Sign out
               </Link>
